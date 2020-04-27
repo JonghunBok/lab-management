aca5efdf4de5b6ca80c431d9056c7171896aa90f
박종훈 <jonghun.park.194@gmail.com>
2020-04-27 17:12:46 +0900

    Update nvidia-driver-install.sh

--- nvidia-driver-install.sh
+++ nvidia-driver-install.sh
@@ -1,5 +1,6 @@
 sudo add-apt-repository ppa:graphics-drivers/ppa -y
 sudo apt update
-sudo apt install nvidia-driver-440 -y
+sudo apt install nvidia-utils-440  -y
+sudo apt install nvidia-driver-440  -y
 
 # sudo reboot
